2cf7b21d782d52af798e6b0efe93a69de84d6a6f
Xinhong Deng <sandra.deng.0520@gmail.com>
2020-01-30 20:49:08 -0500

    init project

---
+++ src/DNSClient.java
@@ -0,0 +1,13 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class DNSClient {
+  public DNSClient() {
+    
+  }
+  
+  public void main() {
+    
+  }
+}
